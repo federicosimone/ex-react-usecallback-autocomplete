4d85bd6a71afd57c28a502d7341e2976d0a925a1
federicosimone <federico.simone15@gmail.com>
2026-05-10 12:10:15 +0200

    prodotti filtrati

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [product, setProduct] = useState([])
+  const [filteredProducts, setFilteredProducts] = useState([])
   const [search, setSearch] = useState("")
 
   console.log("Ricerca", search)
-  console.log("Prodotti trovati", product)
+  console.log("Prodotti trovati", filteredProducts)
 
   const API_URL = "http://localhost:3333"
 
@@ -14,18 +14,29 @@ function App() {
     fetch(`${API_URL}/products?search=${search}`)
       .then(res => res.json())
       .then(data => {
-        setProduct(data);
+        setFilteredProducts(data);
       })
       .catch(err => console.error(err))
 
   }, [search])
 
+  const prodottiDaMostrare = search ? filteredProducts : [];
 
 
   return (
     <>
       <div>
+        <h1>Cerca il prodotto che vuoi: </h1>
         <input type="text" value={search} onChange={e => setSearch(e.target.value)} />
+        <p>Stai cercando: {search}</p>
+        <div>
+          <h2>Suggerimenti:</h2>
+          <ul>
+            {prodottiDaMostrare.map((product) => {
+              return <li key={product.id}>{product.name}</li>
+            })}
+          </ul>
+        </div>
       </div>
 
     </>
